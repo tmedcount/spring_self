54f765f9569175887c30859e912d3feb29d5d237
yamto <yamto@222.118.73.184>
2022-02-15 18:47:39 +0900

    ┌프로퍼티 파일을 이용한 설정 방법

--- src/myspring/di/annot/Hello.java
+++ src/myspring/di/annot/Hello.java
@@ -2,18 +2,22 @@ package myspring.di.annot;
 
 import java.util.List;
 
+import javax.annotation.Resource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Hello {
-	@Value("Spring")
+	@Value("${myname}")
 	private String name;
 	
-	@Autowired
-	@Qualifier("stringPrinter")
+//	@Autowired
+//	@Qualifier("stringPrinter")
+	@Resource(name="${printer1}")
 	private Printer printer;
 	private List<String> names;
 	
